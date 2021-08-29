ad08f5ba6dc2bbf9e37613dcc52872b28f3aadec
Ifeoluwa-Lawal <adeolaoluwafemi18@gmail.com>
2021-08-29 12:40:14 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Prints a text and not integer
+ *
+ * Return: 0 is always (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
