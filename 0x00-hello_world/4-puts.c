2e4d316d47247a301a78ceace04c8763446d727b
Ifeoluwa-Lawal <adeolaoluwafemi18@gmail.com>
2021-08-29 12:21:10 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main: Prints a text
- *
- * Puts: Allows for strings
+ * main - Prints a text and not integer
  *
  * Return: 0 is always
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n ");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }    
