6f61ca8c0fa6d465f0efd616545a102a1e9b645c
Ifeoluwa-Lawal <adeolaoluwafemi18@gmail.com>
2021-08-31 12:08:52 -0700

    FUNCTIONS AND NESTED LOOPS

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * main - Entry Point
- *
- * Return: 0 is always success
+ *main - entry point.
+ *Description: program starts and ends here.
+ *Return: 0 on success.
  */
 int main(void)
 {
-	char text[9] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	return (0);
+char text[9] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
 }
