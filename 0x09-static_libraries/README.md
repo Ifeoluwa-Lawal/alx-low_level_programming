0969aa33914da25994999864f7bc9f4b7c49da98
Ifeoluwa-Lawal <adeolaoluwafemi18@gmail.com>
2021-09-22 09:30:57 -0700

    STATIC LIBRARIES

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+##This is a project on Static Libraries##
+
+#It contains sections on how to create static libraries and how to use static libraries#
---
+++ 0x09-static_libraries/libmy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * This is an empty code with prototype function
+ *
+ * File name - 4-isalpha.c
+ */
+int _isalpha(int c)
+{
+
+}
