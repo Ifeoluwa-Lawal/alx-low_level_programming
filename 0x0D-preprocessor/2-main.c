fd64de1744560346e0d1cdd2870d9d21d51bdbd9
Ifeoluwa-Lawal <adeolaoluwafemi18@gmail.com>
2021-09-28 08:47:56 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * File - Program that prints the name of the file its compiled from
+ *
+ * Return - Always Success
+ */
+int main(void)
+{
+	printf("%s'\n'", __FILE__);
+	return (0);
+}
