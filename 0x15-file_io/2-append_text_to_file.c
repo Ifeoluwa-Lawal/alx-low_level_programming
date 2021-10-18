a0d288c940198a6548ac12900f876bec7b0ebffb
Ifeoluwa-Lawal <adeolaoluwafemi18@gmail.com>
2021-10-18 10:42:26 -0700

    FILE I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * Filename - append_text_to_file
+ *
+ * Description - This function appends text to the end of a file. Just like 'echo'
+ *
+ * Return: Always 0
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int ID = 0, wr = 0, count = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count] != '\0'; count++)
+		{}
+	}
+	ID = open(filename, 0_APPEND | 0_WRONLY, 0600);
+	if (ID == -1)
+	{
+		return (-1);
+	}
+	wr = write(ID, text_content, count);
+	if (wr == -1)
+	{
+		return (-1);
+	}
+	close(ID);
+	return (1);
+}
